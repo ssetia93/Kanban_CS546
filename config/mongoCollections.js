8537756223e5f8b2bde11d53e3b5a3b5e816f3f7
AjayShankar87 <aarumuga@stevens.edu>
2016-11-18 01:12:52 -0500

    commit all

---
+++ config/mongoCollections.js
@@ -0,0 +1,22 @@
+
+const dbConnection = require("./mongoConnection");
+
+
+let getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return () => {
+        if (!_col) {
+            _col = dbConnection().then(db => {
+                return db.collection(collection);
+            });
+        }
+
+        return _col;
+    }
+}
+
+
+module.exports = {
+users: getCollectionFn("users")
+};
