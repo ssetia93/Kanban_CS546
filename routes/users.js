edf408cc24401f8395871fe2132fd97c93ee54f9
AjayShankar87 <aarumuga@stevens.edu>
2016-11-22 14:32:48 -0500

    commit all

--- routes/users.js
+++ routes/users.js
@@ -3,3 +3,32 @@ var express = require('express');
 var router = express.Router();
 var data = require("../data");
 var userData = data.users;
+
+router.get("/users", (req, res) => {
+    userData.getAllUsers().then((userList) => {
+        res.json(userList);
+    }).catch((e) => {
+        res.status(500).json({ error: e });
+    });
+});
+
+router.get("/:id", (req, res) => {
+    userData.getUserById(req.params.id).then((user) => {
+        res.json(user);
+    }).catch(() => {
+        res.status(404).json({ error: "Recipe not found" });
+    });
+});
+
+router.post("/", (req, res) => {
+    var newuserData = req.body; // getting the request body from the post data 
+
+    userData.addUser(newuserData.firstname, newuserData.lastname, newuserData.email, newuserData.occupation)
+        .then((newUser) => {
+            res.json(newUser);
+        }).catch((e) => {
+            res.status(500).json({ error: e });
+        });
+});
+
+module.exports = router;
