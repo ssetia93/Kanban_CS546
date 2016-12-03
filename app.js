b3797a4d84d963ce5a7472f4bd6c6498937669b5
AjayShankar87 <aarumuga@stevens.edu>
2016-12-03 17:43:40 -0500

    commit

--- app.js
+++ app.js
@@ -11,11 +11,31 @@ const cookieParser = require("cookie-parser");
 
 const session      = require('express-session');
 
+const static = express.static(__dirname + '/public');
+
 var configRoutes = require("./routes");
 
 
+app.use("/public", static);
+
+app.use(cookieParser()); 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(session({
+    secret: 'secret', 
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash()); 
+
+
 configRoutes(app);
+
+
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
