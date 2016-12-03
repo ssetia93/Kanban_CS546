727fccf81a0dc650f7dfcd0b68add4d5c83ae66c
AjayShankar87 <aarumuga@stevens.edu>
2016-12-03 17:37:44 -0500

    commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const session      = require('express-session');
+const flash    = require("connect-flash");
+const bcrypt   = require('bcrypt-nodejs');
 var app = express();
 var configRoutes = require("./routes");
 
