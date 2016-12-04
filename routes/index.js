b20947babbdbe824a1068d0e61517a21f2cf9d51
AjayShankar87 <aarumuga@stevens.edu>
2016-12-03 20:38:47 -0500

    commit middleware in index.js routes

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ var constructorMethod = (app) =>
       app.get('/signin', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('auth/login', { message: req.flash('loginMessage') }); 
+        res.render('auth/signin', { message: req.flash('loginMessage') }); 
     });
 
       app.get('/signup', function(req, res) {
@@ -23,12 +23,24 @@ var constructorMethod = (app) =>
         res.render('auth/signup', { message: req.flash('signupMessage') });
     });
 
-     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('private.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
+   app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
     });
 
+     // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/private', // redirect to the secure private section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+      // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/private', // redirect to the secure private section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
     app.use("*", (req, res) => 
     {
         res.sendStatus(404);
