76788a9aa2665a78f739a25d3a85ddcaf61b732f
AjayShankar87 <aarumuga@stevens.edu>
2016-12-03 20:40:48 -0500

    changes in index routes

--- routes/index.js
+++ routes/index.js
@@ -30,21 +30,35 @@ var constructorMethod = (app) =>
 
      // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/private', // redirect to the secure private section
+        successRedirect : '/home', // redirect to the secure private section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
       // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/private', // redirect to the secure private section
+        successRedirect : '/home', // redirect to the secure private section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
+
+
     app.use("*", (req, res) => 
     {
         res.sendStatus(404);
     })
 };
+
     
 module.exports = constructorMethod;
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
